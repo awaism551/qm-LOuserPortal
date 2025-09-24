087f40700e40400fb7112a0bc922fad8fb5d2d26
Farhat Rahamani <frahamani@QMITIMAC01.local>
2025-09-24 13:16:28 +0300

    Fixed Scrolling Issues

--- src/app/[locale]/designs/accounts-benefits/page.tsx
+++ src/app/[locale]/designs/accounts-benefits/page.tsx
@@ -17,12 +17,18 @@ export default function Page(): React.JSX.Element {
           className="inline-flex flex-col justify-start items-start gap-3"
         >
           <div
-            className={`justify-start ${activeTab == "tab1" ? "text-Typography-White" : "text-typography-white-64"} text-base font-normal font-['GT_America'] leading-tight cursor-pointer`}
+            className={`justify-start ${
+              activeTab == "tab1"
+                ? "text-Typography-White"
+                : "text-typography-white-64"
+            } text-base font-normal font-['GT_America'] leading-tight cursor-pointer`}
             onClick={() => setActiveTab("tab1")}
           >
             Account Details
           </div>
-         {activeTab == "tab1" && ( <div className="self-stretch h-0 outline outline-[1.50px] outline-offset-[-0.75px] outline-Stroke-Brand"></div> )}
+          {activeTab == "tab1" && (
+            <div className="self-stretch h-0 outline outline-[1.50px] outline-offset-[-0.75px] outline-Stroke-Brand"></div>
+          )}
         </div>
         <div
           data-color="Purple BG"
@@ -30,12 +36,18 @@ export default function Page(): React.JSX.Element {
           className="h-8 inline-flex flex-col justify-start items-start gap-3"
         >
           <div
-            className={`justify-start ${activeTab == "tab2" ? "text-Typography-White" : "text-typography-white-64"} text-base font-normal font-['GT_America'] leading-tight cursor-pointer`}
+            className={`justify-start ${
+              activeTab == "tab2"
+                ? "text-Typography-White"
+                : "text-typography-white-64"
+            } text-base font-normal font-['GT_America'] leading-tight cursor-pointer`}
             onClick={() => setActiveTab("tab2")}
           >
             Membership Benefits
           </div>
-          {activeTab == "tab2" && ( <div className="self-stretch h-0 outline outline-[1.50px] outline-offset-[-0.75px] outline-Stroke-Brand"></div> )}
+          {activeTab == "tab2" && (
+            <div className="self-stretch h-0 outline outline-[1.50px] outline-offset-[-0.75px] outline-Stroke-Brand"></div>
+          )}
         </div>
       </div>
       {activeTab == "tab1" ? <AccountDetails /> : <MembershipDetails />}
