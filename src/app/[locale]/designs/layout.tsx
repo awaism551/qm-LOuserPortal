d9462454c833038e07d46a8336a154b597744573
Farhat Rahamani <frahamani@QMITIMAC01.local>
2025-09-29 09:16:56 +0300

    Fixed Build Issues

--- src/app/[locale]/designs/layout.tsx
+++ src/app/[locale]/designs/layout.tsx
@@ -35,7 +35,7 @@ export default async function RootLayout({ children, params }: Props) {
   // Load translation file directly in the layout
   let messages;
   try {
-    messages = (await import(`../locales/${locale}.json`)).default;
+    messages = (await import(`../../locales/${locale}.json`)).default;
   } catch {
     messages = {}; // fallback if translation not found
   }
--- src/app/[locale]/login-signup/layout.tsx
+++ src/app/[locale]/login-signup/layout.tsx
@@ -34,7 +34,7 @@ export default async function RootLayout({ children, params }: Props) {
   // Load translation file directly in the layout
   let messages;
   try {
-    messages = (await import(`../locales/${locale}.json`)).default;
+    messages = (await import(`../../locales/${locale}.json`)).default;
   } catch {
     messages = {}; // fallback if translation not found
   }
